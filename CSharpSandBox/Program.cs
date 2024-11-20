aca6eb69911d78e6954c7c46e545c44397ab604e
sebastianhernandez <sebaoctavio@gmail.com>
2024-11-20 07:54:23 -0500

    initial commit with C# devkit

---
+++ CSharpSandBox/Program.cs
@@ -0,0 +1,11 @@
+﻿/// <summary>
+/// A project where we explore C#
+/// </summary>
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World! Welcome to C# Sandbox");
+    }
+}
